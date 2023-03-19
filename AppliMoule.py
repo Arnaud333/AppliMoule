73e0a1a943e1eef98a78a022172bbaf71fd7bf4b
Arnaud333 <128181974+Arnaud333@users.noreply.github.com>
2023-03-19 17:57:05 +0100

    Update AppliMoule.py

--- AppliMoule.py
+++ AppliMoule.py
@@ -40,7 +40,7 @@ st.markdown("""
 # con.close()
 
 selected = option_menu(
-    menu_title='AppliMoules,______________(démo pour JMT)',
+    menu_title='Applicaton de gestion des moules (démo pour JMT)',
     options=["Gestion Moules", "Export Table","Scan (test)"],
     icons=['boxes', 'download','upc-scan'],
     default_index=0,
