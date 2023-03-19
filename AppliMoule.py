400a8b9b1cd5774f9a54ec682a38f9a74b915a10
Arnaud333 <128181974+Arnaud333@users.noreply.github.com>
2023-03-19 17:45:45 +0100

    Update AppliMoule.py

--- AppliMoule.py
+++ AppliMoule.py
@@ -17,6 +17,16 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
 
 # con = sqlite3.connect("Moules.db")
 # cur = con.cursor()
